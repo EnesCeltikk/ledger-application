f457ca86629c84bae432b846a212eb2286682f60
Enes CELTIK <enes.celtik@hotmail.com>
2024-03-20 11:40:23 +0300

    Added transfer balance from user to another user and withdraw functionality

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -144,3 +144,151 @@ func GetAllUsersBalance(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(response)
 }
+
+func TransferBalance(w http.ResponseWriter, r *http.Request) {
+	var sender, receiver database.User
+
+	var transferPayload struct {
+		SenderID   int     `json:"sender_id"`
+		ReceiverID int     `json:"receiver_id"`
+		Amount     float64 `json:"amount"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&transferPayload); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
+	if err := database.Db.First(&sender, transferPayload.SenderID).Error; err != nil {
+		http.Error(w, "Sender not found", http.StatusBadRequest)
+
+		return
+	}
+
+	if err := database.Db.First(&receiver, transferPayload.ReceiverID).Error; err != nil {
+		http.Error(w, "Receiver not found", http.StatusBadRequest)
+
+		return
+	}
+
+	str := fmt.Sprintf("%.10f", transferPayload.Amount)
+	decimalPoints := len(str) - strings.Index(str, ".") - 1
+
+	if decimalPoints > 2 {
+		http.Error(w, "Transfer amount should be a multiple of 0.01", http.StatusBadRequest)
+
+		return
+	}
+
+	if transferPayload.Amount <= 0 {
+		http.Error(w, "Transfer amount should be greater than zero", http.StatusBadRequest)
+
+		return
+	}
+
+	if sender.Balance < transferPayload.Amount {
+		http.Error(w, "Insufficient balance", http.StatusBadRequest)
+
+		return
+	}
+
+	tx := database.Db.Begin()
+
+	oldSenderBalance := sender.Balance
+	newSenderBalance := sender.Balance - transferPayload.Amount
+	if err := tx.Model(&sender).Update("balance", newSenderBalance).Error; err != nil {
+		tx.Rollback()
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	oldReceiverBalance := receiver.Balance
+	newReceiverBalance := receiver.Balance + transferPayload.Amount
+	if err := tx.Model(&receiver).Update("balance", newReceiverBalance).Error; err != nil {
+		tx.Rollback()
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	transaction := database.Transaction{
+		SenderId:           transferPayload.SenderID,
+		ReceiverId:         transferPayload.ReceiverID,
+		Type:               "transfer",
+		Amount:             transferPayload.Amount,
+		SenderOldBalance:   oldSenderBalance,
+		SenderNewBalance:   newSenderBalance,
+		ReceiverOldBalance: oldReceiverBalance,
+		ReceiverNewBalance: newReceiverBalance,
+	}
+
+	if err := tx.Create(&transaction).Error; err != nil {
+		tx.Rollback()
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	fmt.Fprintf(w, "Transfer of %f from user %d to user %d successful", transferPayload.Amount, transferPayload.SenderID, transferPayload.ReceiverID)
+}
+
+func Withdraw(w http.ResponseWriter, r *http.Request) {
+	var user database.User
+
+	var requestPayload struct {
+		Id     int     `json:"id"`
+		Amount float64 `json:"amount"`
+	}
+
+	email := r.FormValue("email")
+
+	if err := database.Db.Where("email = ?", email).First(&user).Error; err != nil {
+		http.Error(w, "User not found", http.StatusBadRequest)
+		return
+	}
+
+	password := r.FormValue("password")
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		http.Error(w, "Invalid password", http.StatusBadRequest)
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestPayload); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
+	if err := database.Db.First(&user, requestPayload.Id).Error; err != nil {
+		http.Error(w, "User not found", http.StatusBadRequest)
+
+		return
+	}
+
+	if user.Balance < requestPayload.Amount {
+		http.Error(w, "Insufficient balance", http.StatusBadRequest)
+
+		return
+	}
+
+	newBalance := user.Balance - requestPayload.Amount
+	if err := database.Db.Model(&user).Update("balance", newBalance).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	fmt.Fprintf(w, "User %d balance updated successfully to %f", user.Id, newBalance)
+}
--- backend/router/router.go
+++ backend/router/router.go
@@ -15,6 +15,8 @@ func Start() {
 	router.HandleFunc("/add-balance-to-user", handlers.AddBalanceToUser).Methods("POST")
 	router.HandleFunc("/get-balance-of-user/{id}", handlers.GetBalanceOfUser).Methods("GET")
 	router.HandleFunc("/get-all-users-balance", handlers.GetAllUsersBalance).Methods("GET")
+	router.HandleFunc("/transfer-balance", handlers.TransferBalance).Methods("POST")
+	router.HandleFunc("/withdraw", handlers.Withdraw).Methods("POST")
 
 	http.ListenAndServe(":8080", router)
 }
